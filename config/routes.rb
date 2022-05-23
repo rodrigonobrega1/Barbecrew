71fd92880bd20c5356bc14a23dc1576f69fafb73
sayonara silva <sayow@hotmail.com>
2022-05-23 17:08:45 +0100

    atualização do routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :barbecues only: [ :new, :create ]
+  end
   root to: 'pages#home'
   resources :barbecues
 end
