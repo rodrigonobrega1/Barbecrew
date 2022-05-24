8b4d613f29c5eaa7247512438e83b64ebfc9635a
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-05-24 17:42:08 +0100

    final do dia 24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show]
+
   def profile
     @user = current_user
   end
+
+  # GET /trainers
+  def index
+    @user = User.all
+  end
+
+  # GET /trainers/1
+  def show
+    @barbecue = Barbecue.new
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:email)
+    end
+
+
 end
