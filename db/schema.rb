6c353e0e7fdacd09572ec6756b45fe6400f6d53e
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-05-25 15:57:32 +0100

    update map not work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_123014) do
+ActiveRecord::Schema.define(version: 2022_05_25_142909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2022_05_24_123014) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "picture"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_barbecues_on_user_id"
   end
 
