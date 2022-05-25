bec70cfb68a70ed10216f4bde4681e0c0cc244a2
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-05-25 11:32:20 +0100

    update conflict bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,3 @@
 class BookingsController < ApplicationController
-  def create
-    @booking = Booking.new
-    @booking = @booking.barbecue.create(booking_params)
-    if @booking.save
-      # TODO
-    end
-  end
 
-  private
-
-  def booking_params
-    params.require(:booking).permit(:user_id, :barbecue_id)
-  end
 end
