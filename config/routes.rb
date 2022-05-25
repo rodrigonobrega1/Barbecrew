a3f41b0e683473ccb8a09a07b6d86436d1028fca
sayonara silva <sayow@hotmail.com>
2022-05-25 10:47:47 +0100

    criação do new html e da autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'barbecues#index'
+  resources :barbecues
   devise_for :users do
   end
-  resources :barbecues
+
   resources :bookings
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
