50996b7197ed1b0e148e134a0930741668960106
Carlos Soto <soto.simas@gmail.com>
2022-05-25 11:16:29 +0100

    booking controller, check it out

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,37 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: :destroy
+  before_action :set_user
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
   def create
-    @booking = Booking.new
-    @booking = @booking.barbecue.create(booking_params)
-    if @booking.save
+    @booking = Booking.new(booking_params)
+    @bookmark.user = @user
+    if @bookmark.save
+      redirect_to list_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to user_path(@user)
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:user_id, :barbecue_id)
+  end
+
+  def set_barbecue
+    @barbecue = Barbecue.find(params[:id])
+  end
+
+  def set_user
+    @user = user.find(params[:user_id])
   end
 end
