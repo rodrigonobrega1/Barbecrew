1b6a923f75bf26053ae522c86c7dbc6ea07b788d
sayonara silva <sayow@hotmail.com>
2022-05-27 14:29:04 +0100

    validates ok

--- app/models/barbecue.rb
+++ app/models/barbecue.rb
@@ -3,6 +3,12 @@ class Barbecue < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  validates :barbecue_model, presence: true
+  validates :barbecue_type, presence: true
+  validates :barbecue_price, presence: true, numericality: { only_integer: true }
+  validates :barbecue_description, length: { maximum: 250 }
+  validates :barbecue_location, presence: true
+
   geocoded_by :barbecue_location
   after_validation :geocode, if: :will_save_change_to_barbecue_location?
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   has_many :barbecues
   has_many :bookings
 
-  
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+
 end
