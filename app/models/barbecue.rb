1bf73c10649102253f8ab85acf653e2397777da8
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-05-27 12:10:28 +0100

    update pictures

--- app/models/barbecue.rb
+++ app/models/barbecue.rb
@@ -1,10 +1,8 @@
 class Barbecue < ApplicationRecord
-
   has_one_attached :picture
   belongs_to :user
   has_many :bookings
 
   geocoded_by :barbecue_location
   after_validation :geocode, if: :will_save_change_to_barbecue_location?
-
 end
